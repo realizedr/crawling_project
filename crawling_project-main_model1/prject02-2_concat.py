af9cdb893ef4bff01fd8ae2419a7e4f1787dcd22
ASIA-04 <realizedr@gmail.com>
2022-06-02 12:32:05 +0900

    모델 1 데이터 2-2 concat

--- crawling_project-main_model1/prject02-2_concat.py
+++ crawling_project-main_model1/prject02-2_concat.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 import datetime
 
-data_path = glob.glob('./crawling_data(2)/*')
+data_path = glob.glob('./crawling_data(2)/*')                   #glob 모듈의 glob 함수는 사용자가 제시한 조건에 맞는 파일명을 리스트 형식으로 반환한다.
 print(data_path)
 
 df = pd.DataFrame()
@@ -10,7 +10,7 @@ for path in data_path[1:] :
     df_temp = pd.read_csv(path) #임시 저장
     df = pd.concat([df, df_temp])
 df.dropna(inplace=True) #Nane 값 제거
-df.reset_index(inplace=True,drop = True) #제거했으니 reset #인덱스 있는거 합칠 땐 drop = True
+df.reset_index(inplace=True,drop = True)                        #제거했으니 reset #인덱스 있는거 합칠 땐 drop = True
 print(df.head())
 print(df.tail())
 df.info()
--- crawling_project-main_model1/project02_3_processing.py
+++ crawling_project-main_model1/project02_3_processing.py
@@ -8,19 +8,24 @@ import re
 
 import sys
 
-df_read_csv = pd.read_csv('./예측데이터/wadiz_data_예측데이터_20220530.csv')
-print(df_read_csv)
+df_read_csv = pd.read_csv('./예측데이터/wadiz_data_예측데이터_20220530.csv')        # 전처리 전의 예측데이터를 불러 온다.
+print(df_read_csv.head())
+
+
 titles = []
 categories = []
 for title in df_read_csv['title'] :
-    title = re.compile('[^가-힣 ]').sub(' ', title)
+    title = re.compile('[^가-힣 ]').sub(' ', title)                              # 한글과 띄어쓰기를 제외한 모든 부분을 제거한다.
     titles.append(title)
+print(titles)
+
 df_section_titles = pd.DataFrame(titles, columns=['title'])
-for title in df_read_csv['category'] :
-    categories.append(title)
+for i in df_read_csv['category'] :
+    categories.append(i)
 df_section_title = pd.DataFrame(categories, columns=['category'])
 
-df_titles = pd.concat([df_section_titles, df_section_title], axis=1)
+df_titles = pd.concat([df_section_titles, df_section_title], axis=1)             # axis=1은 컬럼끼리 더하게 한다.
+
 # print(df_titles.head())
 # df_titles.info()
 
@@ -33,7 +38,7 @@ df_titles = pd.concat([df_section_titles, df_section_title], axis=1)
 # df_titles = title_tag(df_read_csv)
 
 df_titles.to_csv('./예측데이터/wadiz_예측데이터_{}.csv'.format(
-    datetime.datetime.now().strftime('%Y%m%d')), index=False)
+    datetime.datetime.now().strftime('%Y%m%d')), index=False)                    #
 
 
 
